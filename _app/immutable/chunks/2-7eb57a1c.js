377eeb614a89eabf324e324d045d3b1c02790b23
Andrew Dykema <62975486+atdykema@users.noreply.github.com>
2023-01-23 11:00:36 -0500

    Updates

--- _app/immutable/chunks/2-7eb57a1c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c535ef7f.js";export{t as component};
---
+++ _app/immutable/chunks/5-4f083b39.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/projects/_page.svelte-6d5d6752.js";export{t as component};
